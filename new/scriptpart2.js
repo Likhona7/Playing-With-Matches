f1355b60e629b0e45b60c71f8d34eacc1a61dc12
likhona <likhonapr@projectcodex.co>
2016-03-15 10:57:44 +0200

    new work

--- new/scriptpart2.js
+++ new/scriptpart2.js
@@ -1,8 +1,9 @@
 var random = function(max) {
   return Math.floor(Math.random() * max) + 1;
 };
+
 //get all the number div's
-var numbers = document.querySelectorAll(".number", "diagonal1", "diagonal2", "column1", "column2", "column3");
+var numbers = document.querySelectorAll(".number");
 //loop through all the div's and allocate a random number
 for (var i = 0; i < numbers.length; i++) {
   var number = numbers[i];
@@ -12,17 +13,20 @@ for (var i = 0; i < numbers.length; i++) {
 var matchRow = function(base) {
   var matchCount = 0;
   if (numbers[base].innerHTML === numbers[base + 1].innerHTML) {
+    document.getElementById("rowMatch").innerHTML = " match ";
     numbers[base].classList.add("highlight");
     numbers[base + 1].classList.add("highlight");
     matchCount++;
   }
   if (numbers[base + 1].innerHTML === numbers[base + 2].innerHTML) {
+   document.getElementById("rowMatch").innerHTML =  " match ";
     //alert("bottom match!");
     numbers[base + 1].classList.add("highlight");
     numbers[base + 2].classList.add("highlight");
     matchCount++;
   }
   if (numbers[base].innerHTML === numbers[base + 2].innerHTML) {
+    document.getElementById("rowMatch").innerHTML =  " match ";
     //alert("all match!");
     numbers[base].classList.add("highlight");
     numbers[base + 2].classList.add("highlight");
@@ -34,16 +38,19 @@ var matchRow = function(base) {
 var matchColumn = function(base) {
   var matchCount = 0;
   if (numbers[base].innerHTML === numbers[base + 3].innerHTML) {
+    document.getElementById("columnMatch").innerHTML = " match ";
     numbers[base].classList.add("highlightColumn");
     numbers[base + 3].classList.add("highlightColumn");
     matchCount++;
   }
   if (numbers[base + 3].innerHTML === numbers[base + 6].innerHTML) {
+    document.getElementById("columnMatch").innerHTML = " match ";
     numbers[base + 3].classList.add("highlightColumn");
     numbers[base + 6].classList.add("highlightColumn");
     matchCount++;
   }
   if (numbers[base].innerHTML === numbers[base + 6].innerHTML) {
+    document.getElementById("columnMatch").innerHTML = " match ";
     numbers[base].classList.add("highlightColumn");
     numbers[base + 6].classList.add("highlightColumn");
     matchCount++;
@@ -53,16 +60,19 @@ var matchColumn = function(base) {
 var matchDiagonal1 = function(base) {
   var matchCount = 0;
   if (numbers[base].innerHTML === numbers[base + 4].innerHTML) {
+    document.getElementById("diagnalMatch").innerHTML = " match ";
     numbers[base].classList.add("highlightDiagonal");
     numbers[base + 4].classList.add("highlightDiagonal");
     matchCount++;
-   }
+  }
   if (numbers[base + 4].innerHTML === numbers[base + 8].innerHTML) {
+    document.getElementById("diagnalMatch").innerHTML = " match ";
     numbers[base].classList.add("highlightDiagonal");
     numbers[base + 4].classList.add("highlightDiagonal");
     matchCount++;
   }
   if (numbers[base].innerHTML === numbers[base + 8].innerHTML) {
+    document.getElementById("diagnalMatch").innerHTML = " match ";
     numbers[base].classList.add("highlightDiagonal");
     numbers[base + 4].classList.add("highlightDiagonal");
     //  matchCount++;
@@ -72,24 +82,27 @@ var matchDiagonal1 = function(base) {
 var matchDiagonal2 = function(base) {
   var matchCount = 0;
   if (numbers[base + 2].innerHTML === numbers[base + 4].innerHTML) {
+    document.getElementById("diagnalMatch").innerHTML = " match ";
     numbers[base + 2].classList.add("highlightDiagonal");
     numbers[base + 4].classList.add("highlightDiagonal");
     matchCount++;
   }
   if (numbers[base + 4].innerHTML === numbers[base + 6].innerHTML) {
+    document.getElementById("diagnalMatch").innerHTML = " match ";
     numbers[base + 4].classList.add("highlightDiagonal");
     numbers[base + 6].classList.add("highlightDiagonal");
     matchCount++;
   }
   if (numbers[base + 2].innerHTML === numbers[base + 6].innerHTML) {
+    document.getElementById("diagnalMatch").innerHTML = " match ";
     numbers[base + 2].classList.add("highlightDiagonal");
     numbers[base + 6].classList.add("highlightDiagonal");
     matchCount++;
   }
   return matchCount;
 };
- var matches = matchDiagonal1(0);
- var matches = matchDiagonal2(2);
+var matches = matchDiagonal1(0);
+var matches = matchDiagonal2(2);
 
 var matches = matchColumn(0);
 var matches = matchColumn(1);
